10bbb8937a484032611e336f9d33f15d5f879ca9
samanwaysm <samanwaysm@gmail.com>
2024-11-02 12:08:12 +0530

    2-10-24 first commit 1

--- server/controller/userSide/addressController.js
+++ server/controller/userSide/addressController.js
@@ -206,6 +206,8 @@ exports.getAddress = async (req, res) => {
 
 exports.updateAddress = async (req, res) => {    
     const userId = req.query.userId;
+    console.log(req.body);
+    
     const { addressId, updatedData } = req.body;
     const { username, phone, street, block, unitnum, postal } = updatedData;
 
